0242e89bbc946daaf6208e98891e46c93209ba48
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-06-22 19:32:40 +0300

    Application has database improvements.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import lowdb from 'lowdb'
-import FileSync from 'lowdb/adapters/FileAsync'
 import { getTwitterFollowers, getInstagramFollowers } from './lib/scaper'
+import db from './lib/db'
 
 const app = express()
+
+console.log(db)
+
 app.get('/scaper', async (req, res, next) => {
 	console.log('Hello world')
 	const promiseTwitterFollowers = getTwitterFollowers('https://twitter.com/wesbos')
