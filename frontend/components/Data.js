6155542544ce333c02b4a70c680dc375eb93ffc5
Dimitar Danailov <dimityr.danailov@gmail.com>
2019-06-24 16:44:31 +0300

    Front-end application is able to reach backend services.

--- frontend/components/Data.js
+++ frontend/components/Data.js
@@ -2,12 +2,14 @@ import { useContext } from 'react'
 import { ScrapeContext } from './ScrapeContext' 
 
 export default function Data() {
-	const scrapeData = useContext(ScrapeContext)
-	console.log(scrapeData)
+	const { twitterFollowers, instagramFollowers } = useContext(ScrapeContext).scapes
 
 	return (
 		<div>
 			<h2>Your data:</h2>
+
+			<h3>Twitter followers are: { twitterFollowers } </h3>
+			<h3>instagram followers are: { instagramFollowers } </h3>
 		</div>
 	)
 }
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,11 +1,34 @@
+import { useEffect, useState } from 'react'
 import { ScrapeProvider } from './ScrapeContext'
 
+// Custom Hooks
+function usesScrapes() {
+	const [ scrapes, setScrapes ] = useState({
+		twitterFollowers: 0,
+		instagramFollowers: 0
+	})
+
+	useEffect(() => { 
+		const fetchData = async () => {
+			const res = await fetch('http://localhost:2293/data')
+			const data = await res.json()
+
+			setScrapes(data)
+		}
+
+		fetchData()
+	}, [])
+
+	return scrapes
+}
+
 export default function Page({children}) {
+	const scapes = usesScrapes()
+
 	return (
 		<ScrapeProvider
 			value={{
-				hey: 'go',
-				lets: 'go'
+				scapes
 			}}>
 			<div className="page">{children}</div>
 		</ScrapeProvider>
